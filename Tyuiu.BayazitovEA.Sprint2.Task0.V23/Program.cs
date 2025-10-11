4b4ccd129fc6465094b082ef140d2747b5c0e512
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-11 21:30:46 +0500

    Add project files.

---
+++ Tyuiu.BayazitovEA.Sprint2.Task0.V23/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BayazitovEA.Sprint2.Task0.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 105;
+        int y = 795;
+        bool[]res = new bool[6];
+        res=ds.GetCompareOperations(x, y);
+        for (int i = 0; i < 6; i++)
+        { Console.Write(res[i]); }
+        Console.ReadKey();
+    }
+}
