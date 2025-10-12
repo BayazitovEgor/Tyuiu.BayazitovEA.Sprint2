cd2689d2c2f01a05aa572d805f49dd88e59e4611
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-12 20:30:59 +0500

    Task1

---
+++ Tyuiu.BayazitovEA.Sprint2.Task1.V15/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BayazitovEA.Sprint2.Task1.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int a = 415;
+        int b = 616;
+        int c = 134;
+        int d = 127;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a,b,c,d);
+        for (int i = 0; i < 6; i++)
+        { Console.Write(res[i]); }
+        Console.ReadKey();
+    }
+}
