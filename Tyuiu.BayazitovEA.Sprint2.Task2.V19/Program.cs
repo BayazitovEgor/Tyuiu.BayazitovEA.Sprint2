12d4d232586afd0b3121abf8ebd5c4e8c70caf92
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-13 15:35:17 +0500

    Task2

---
+++ Tyuiu.BayazitovEA.Sprint2.Task2.V19/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BayazitovEA.Sprint2.Task2.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        DataService ds = new DataService();
+        bool res = ds.CheckDotInShadedArea(x, y);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
