dd3ffabf18f5eac3b800cde6bc9bc46bd3350024
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-13 16:17:57 +0500

    Task3

---
+++ Tyuiu.BayazitovEA.Sprint2.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BayazitovEA.Sprint2.Task3.V25.Lib;
+namespace Tyuiu.BayazitovEA.Sprint2.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint2.Task3.V25/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.BayazitovEA.Sprint2.Task3.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+        Console.WriteLine(res);
+    }
+}
